2b9328594aa78683ea3f574a59de41763d4021a8
todd25 <ec2-user@ip-172-31-24-250.us-west-1.compute.internal>
2021-09-06 01:21:03 +0000

    liyunlong

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://lh3.googleusercontent.com/proxy/sAuGmJ7JK8ZQpiostGqDviA9U_HkIsYATSuaI0FdoNc1OaZJR2_2Y4EWUjuHjmUqMivb7Tn_lujnqumyCVTU9mdwyWiW" className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>me looking at jordan and lan's mom</code>
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {/* Learn React */}
         </a>
       </header>
     </div>
